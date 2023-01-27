4797912b45157c1c48e370dfa6fe7832a86325c6
k9ne257 <unelteed@hotmail.com>
2023-01-27 17:37:38 +0100

    spring init

---
+++ src/main/java/com/webfrey/uniliga/UniLigaApplication.java
@@ -0,0 +1,13 @@
+package com.webfrey.uniliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniLigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniLigaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webfrey/uniliga/UniLigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webfrey.uniliga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniLigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
