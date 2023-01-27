d137195fd321ad62920d9f8816026a2e0a83cf16
k9ne257 <unelteed@hotmail.com>
2023-01-27 23:06:18 +0100

    merge conflicts fixed

--- src/main/java/com/webfrey/uniliga/entities/Team.java
+++ src/main/java/com/webfrey/uniliga/entities/Team.java
@@ -1,12 +1,6 @@
 package com.webfrey.uniliga.entities;
 
-<<<<<<< HEAD:src/main/java/com/webfrey/uniliga/entities/Teams.java
 import jakarta.persistence.*;
-=======
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Transient;
->>>>>>> 1352dc1cf8ed3ca0de3c07792665341aee22239a:src/main/java/com/webfrey/uniliga/entities/Team.java
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
