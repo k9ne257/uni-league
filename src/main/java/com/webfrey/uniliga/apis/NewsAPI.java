e42bffd83826460191a38c414be3b3078ee3b909
k9ne257 <unelteed@hotmail.com>
2023-01-27 19:56:32 +0100

    ist done

---
+++ src/main/java/com/webfrey/uniliga/apis/NewsAPI.java
@@ -0,0 +1,18 @@
+package com.webfrey.uniliga.apis;
+
+import com.webfrey.uniliga.services.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/news")
+
+public class NewsAPI {
+
+    @Autowired
+    private NewsService newsService;
+
+}
--- src/main/java/com/webfrey/uniliga/services/NewsService.java
+++ src/main/java/com/webfrey/uniliga/services/NewsService.java
@@ -1,7 +1,11 @@
 package com.webfrey.uniliga.services;
 
+import com.webfrey.uniliga.repositories.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewsService {
+    @Autowired
+    private NewsRepository newsRepository;
 }
