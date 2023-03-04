b30418a814eba9d671d278e2b698ad2d9de6781c
k9ne257 <unelteed@hotmail.com>
2023-03-04 06:05:26 +0100

    tests for gameservices

--- src/test/java/com/webfrey/uniliga/services/GameServiceTest.java
+++ src/test/java/com/webfrey/uniliga/services/GameServiceTest.java
@@ -1,14 +1,20 @@
 package com.webfrey.uniliga.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.webfrey.uniliga.entities.Game;
 import com.webfrey.uniliga.entities.Team;
 import com.webfrey.uniliga.repositories.GameRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 
@@ -108,6 +114,139 @@ class GameServiceTest {
         when(teamService.getTeams()).thenReturn(teamList);
         assertEquals(6, gameService.generateSeason().size());
         verify(teamService).getTeams();
+    }
+
+    /**
+     * Method under test: {@link GameService#saveSeason(List)}
+     */
+    @Test
+    void testSaveSeason() {
+        // TODO: Complete this test.
+        //   Reason: R002 Missing observers.
+        //   Diffblue Cover was unable to create an assertion.
+        //   Add getters for the following fields or make them package-private:
+        //     GameService.gameRepository
+        //     GameService.teamService
+
+        gameService.saveSeason(new ArrayList<>());
+    }
+
+    /**
+     * Method under test: {@link GameService#saveSeason(List)}
+     */
+    @Test
+    void testSaveSeason2() {
+        when(gameRepository.save((Game) any())).thenReturn(new Game());
+
+        ArrayList<Game> gameList = new ArrayList<>();
+        gameList.add(new Game());
+        gameService.saveSeason(gameList);
+        verify(gameRepository).save((Game) any());
+    }
+
+    /**
+     * Method under test: {@link GameService#saveSeason(List)}
+     */
+    @Test
+    void testSaveSeason3() {
+        when(gameRepository.save((Game) any())).thenReturn(new Game());
+
+        ArrayList<Game> gameList = new ArrayList<>();
+        gameList.add(new Game());
+        gameList.add(new Game());
+        gameService.saveSeason(gameList);
+        verify(gameRepository, atLeast(1)).save((Game) any());
+    }
+
+    /**
+     * Method under test: {@link GameService#playNextRound(List, int)}
+     */
+    @Test
+    void testPlayNextRound() {
+        assertNull(gameService.playNextRound(new ArrayList<>(), 1));
+        assertTrue(gameService.playNextRound(new ArrayList<>(), -1).isEmpty());
+    }
+
+    /**
+     * Method under test: {@link GameService#playNextRound(List, int)}
+     */
+    @Test
+    void testPlayNextRound2() {
+        ArrayList<Team> teamList = new ArrayList<>();
+        teamList.add(new Team());
+        teamList.add(new Team());
+        assertEquals(1, gameService.playNextRound(teamList, 1).size());
+    }
+
+    /**
+     * Method under test: {@link GameService#playNextRound(List, int)}
+     */
+    @Test
+    void testPlayNextRound3() {
+        ArrayList<Team> teamList = new ArrayList<>();
+        teamList.add(new Team());
+        teamList.add(new Team());
+        teamList.add(new Team());
+        assertEquals(1, gameService.playNextRound(teamList, 1).size());
+        assertEquals(3, teamList.size());
+    }
+
+    /**
+     * Method under test: {@link GameService#playNextRound(List, int)}
+     */
+    @Test
+    @Disabled("TODO: Complete this test")
+    void testPlayNextRound4() {
+        // TODO: Complete this test.
+        //   Reason: R013 No inputs found that don't throw a trivial exception.
+        //   Diffblue Cover tried to run the arrange/act section, but the method under
+        //   test threw
+        //   java.lang.NullPointerException: Cannot invoke "com.webfrey.uniliga.entities.Team.getName()" because "team1" is null
+        //       at com.webfrey.uniliga.services.GameService.playNextRound(GameService.java:65)
+        //   See https://diff.blue/R013 to resolve this issue.
+
+        ArrayList<Team> teamList = new ArrayList<>();
+        teamList.add(null);
+        teamList.add(new Team());
+        gameService.playNextRound(teamList, 1);
+    }
+
+    /**
+     * Method under test: {@link GameService#playNextRound(List, int)}
+     */
+    @Test
+    void testPlayNextRound5() {
+        Team team = mock(Team.class);
+        when(team.getName()).thenReturn("Name");
+
+        ArrayList<Team> teamList = new ArrayList<>();
+        teamList.add(team);
+        teamList.add(new Team());
+        assertEquals(1, gameService.playNextRound(teamList, 1).size());
+        verify(team).getName();
+    }
+
+    /**
+     * Method under test: {@link GameService#playNextRound(List, int)}
+     */
+    @Test
+    @Disabled("TODO: Complete this test")
+    void testPlayNextRound6() {
+        // TODO: Complete this test.
+        //   Reason: R013 No inputs found that don't throw a trivial exception.
+        //   Diffblue Cover tried to run the arrange/act section, but the method under
+        //   test threw
+        //   java.lang.NullPointerException: Cannot invoke "com.webfrey.uniliga.entities.Team.getName()" because "team2" is null
+        //       at com.webfrey.uniliga.services.GameService.playNextRound(GameService.java:66)
+        //   See https://diff.blue/R013 to resolve this issue.
+
+        Team team = mock(Team.class);
+        when(team.getName()).thenReturn("Name");
+
+        ArrayList<Team> teamList = new ArrayList<>();
+        teamList.add(team);
+        teamList.add(null);
+        gameService.playNextRound(teamList, 1);
     }
 }
 
